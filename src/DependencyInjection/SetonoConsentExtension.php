381f2a308a0b26d20bdb8185dafee3054be7e746
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-09 09:00:35 +0200

    Use new bundle directory structure

--- src/DependencyInjection/SetonoConsentExtension.php
+++ src/DependencyInjection/SetonoConsentExtension.php
@@ -29,7 +29,7 @@ final class SetonoConsentExtension extends Extension
 
         $container->setParameter('setono_consent.consents', $consents);
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.xml');
     }
 }
