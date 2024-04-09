a400b0a3eb82f89677717da19d1cc17765dfc041
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-09 08:46:06 +0200

    Remove zombie

--- composer-unused.php
+++ composer-unused.php
@@ -7,7 +7,6 @@ use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 
 return static function (Configuration $config): Configuration {
     return $config
-        ->addNamedFilter(NamedFilter::fromString('setono/client-id-bundle'))
         ->addNamedFilter(NamedFilter::fromString('setono/consent-contracts'))
     ;
 };
